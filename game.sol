0e66d17f2ba61213be8a8bcd33e13de1192ce828
bobomurod <bobomurod.boboev@yandex.ru>
2018-03-09 08:00:09 +0300

    getBalance funciton

--- game.sol
+++ game.sol
@@ -6,13 +6,18 @@ contract game{
 
     }
 
+    function getBalance() public constant returns(uint){
+        return this.balance;
+    }
+    
+
     function play() payable public {
 
         //gameer will need to pay 0.01 ether 
         if (msg.value != 0.01 ether){
             throw;
         }
-        
+
         //gamer gets 0.02 back if numeber coorect
     }
 
